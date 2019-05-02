2016d8552b48cbe482b2a35b1922c12ccd310d42
Angele Louise <angelealst@hotmail.com>
2019-05-02 17:24:10 -0300

    Modificações no layout

---
+++ app/src/main/java/com/ufrn/angele/apotheca/activity/CadastrarPostActivity.java
@@ -0,0 +1,15 @@
+package com.ufrn.angele.apotheca.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ufrn.angele.apotheca.R;
+
+public class CadastrarPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_post);
+    }
+}
---
+++ app/src/main/java/com/ufrn/angele/apotheca/activity/DetalharPostActivity.java
@@ -0,0 +1,15 @@
+package com.ufrn.angele.apotheca.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ufrn.angele.apotheca.R;
+
+public class DetalharPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhar_post);
+    }
+}
