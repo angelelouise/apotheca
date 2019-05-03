171c68751d10fc84e8301dce47d01c9855bd0da9
angelelouise <angelealst@hotmail.com>
2019-05-03 19:28:46 -0300

    Updated: DiscenteDAO e DiscenteRepository

--- app/src/main/java/com/ufrn/angele/apotheca/bd/DiscenteDAO.java
+++ app/src/main/java/com/ufrn/angele/apotheca/bd/DiscenteDAO.java
@@ -1,5 +1,6 @@
 package com.ufrn.angele.apotheca.bd;
 
+import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
@@ -10,19 +11,24 @@ import com.ufrn.angele.apotheca.dominio.Discente;
 
 import java.util.List;
 
+@Dao
 public interface DiscenteDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     public void inserir(Discente discente);
 
     @Update
-    public void atualizar(Discente discente;
+    public void atualizar(Discente discente);
 
     @Delete
     public void deletar(Discente discente);
 
-    @Query("SELECT * FROM usuario ORDER BY id ASC")
+    @Query("SELECT * FROM discente WHERE cpf = :cpf LIMIT 1")
+    public List<Discente> findByCPF(String cpf);
+
+    @Query("SELECT * FROM discente ORDER BY id ASC")
     public List<Discente> buscarTodos();
 
-    @Query("SELECT * FROM usuario WHERE login = :login LIMIT 1")
-    public Discente findByLogin(String login);
+    @Query("SELECT * FROM discente WHERE id_usuario = :id_usuario LIMIT 1")
+    public List<Discente> findByIdUsuario(int id_usuario);
+
 }
--- app/src/main/java/com/ufrn/angele/apotheca/bd/DiscenteRepository.java
+++ app/src/main/java/com/ufrn/angele/apotheca/bd/DiscenteRepository.java
@@ -1,4 +1,58 @@
 package com.ufrn.angele.apotheca.bd;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.ufrn.angele.apotheca.dominio.Discente;
+
+import java.util.List;
+
 public class DiscenteRepository {
+    private List<Discente> discentes;
+    private DiscenteDAO discenteDAO;
+
+    private ConnectivityManager cm;
+
+    public void inserir (Discente discente){
+        new InsertASync(discenteDAO).execute(discente);
+    }
+
+    public List<Discente> findByCPF(String cpf){
+
+        //if(netOn) > consulta usuário no serviço
+        discentes = discenteDAO.findByCPF(cpf);
+        return discentes;
+    }
+
+    public List<Discente> findByIdUsuario(int id_usuario){
+
+        //if(netOn) > consulta usuário no serviço
+        discentes = discenteDAO.findByIdUsuario(id_usuario);
+        return discentes;
+    }
+    private class InsertASync extends AsyncTask<Discente, Void, Void> {
+        private DiscenteDAO discenteDAO;
+
+        public InsertASync (DiscenteDAO discenteDAO){
+            this.discenteDAO= discenteDAO;
+        }
+        @Override
+        protected Void doInBackground(Discente... discente){
+            Discente p = discente[0];
+            try{
+                discenteDAO.inserir(p);
+            }catch (Exception e){
+                Log.e(DiscenteRepository.class.getName(), "ERRO", e);
+            }
+            return null;
+        }
+
+    }
+    private boolean netOn(){
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+    }
 }
